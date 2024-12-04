7c63b9cabb0100a9f8ae8697f470391add62d6f7
DanM-r <dmorarod16@gmail.com>
2024-12-03 19:15:55 -0600

    Added bootstrap and axios for the frontend project

--- frontend/README.md
+++ frontend/README.md
@@ -2,7 +2,7 @@
 
 ## Project setup
 ```
-npm install
+npm install bootstrap axios
 ```
 
 ### Compiles and hot-reloads for development
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import bootstrap from 'bootstrap';
+
+// import '../scss/custom.css'
+
+const app = createApp(App);
+
+app.use(bootstrap);
+app.mount('#app')
