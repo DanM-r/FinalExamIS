bd30969e7045668dd145fc45e9721c63d954bfa0
DanM-r <dmorarod16@gmail.com>
2024-12-04 10:04:30 -0600

    Added logic for adding coffees to order and adding cash

--- frontend/src/components/VendingMachine.vue
+++ frontend/src/components/VendingMachine.vue
@@ -34,15 +34,13 @@
     <div class="flex-fill p-4" style="height: 98vh;">
       <div>
         <h3 class="ff-agrandirbold fw-bold">Seleccion cafes por comprar</h3>
-        <form>
-          <label>Tipo de cafe</label>
-          <select>
-            <option v-for="coffee in coffees" v-bind:key="coffee.name">{{ coffee.name }}</option>
-          </select>
-          <label>Cantidad de cafes</label>
-          <input type="text" value="0">
-          <button type="submit">Agregar</button>
-        </form>
+        <label>Tipo de cafe</label>
+        <select v-model="orderedCoffeeName">
+          <option v-for="coffee in coffees" v-bind:key="coffee.name">{{ coffee.name }}</option>
+        </select>
+        <label>Cantidad de cafes</label>
+        <input type="text" v-model="quantityOrdered">
+        <button @click="addToOrder">Agregar</button>
       </div><hr>
       <div>
         <h3 class="ff-agrandirbold fw-bold">Vista de la Orden</h3>
@@ -62,7 +60,7 @@
             </div>
           </div>
           <div class="rounded-2 bg-secondary">
-            <div v-for="orderedCoffee in orderCoffees" v-bind:key="orderedCoffee.id" class="row">
+            <div v-for="orderedCoffee in orderedCoffees" v-bind:key="orderedCoffee.id" class="row">
               <div class="col">{{ orderedCoffee.quantity }}</div>
               <div class="col">{{ this.coffees.find(coffee => coffee.id === orderedCoffee.id).name }}</div>
               <div class="col">₡ {{ this.coffees.find(coffee => coffee.id === orderedCoffee.id).price }}</div>
@@ -85,20 +83,18 @@
       </div><hr>
       <div>
         <h3 class="ff-agrandirbold fw-bold">Realizar el pago</h3>
-        <form>
-          <label>Moneda o billete</label>
-          <select>
-            <option v-for="coin in coins" v-bind:key="coin.value">₡ {{ coin.value }}</option>
-          </select>
-          <label>Cantidad</label>
-          <input type="text" value="0">
-          <button type="submit">Agregar</button>
-        </form>
+        <label>Moneda o billete</label>
+        <select v-model="cashValueSelected">
+          <option v-for="coin in coins" v-bind:key="coin.value">₡ {{ coin.value }}</option>
+        </select>
+        <label>Cantidad</label>
+        <input type="text" v-model="quantityCash">
+        <button @click="addCash">Agregar</button>
         <div>
           <span>Falta de cancelar: ₡ {{ this.receiptDetails.remainingToPay }}</span><br>
-          <span>Vuelto: ₡ {{ calculateChange() }}</span>
+          <span>Vuelto: ₡ {{ this.receiptDetails.change }}</span>
         </div>
-        <button>Pagar</button>
+        <button @click="pay">Pagar</button>
       </div>
     </div>
   </div>
@@ -130,36 +126,41 @@ export default {
           price: 1200.00,
         }
       ],
-      orderCoffees: [
+      orderedCoffees: [
         { id: 1, quantity: 2 },
         { id: 2, quantity: 1 },
       ],
-      receiptDetails: {
-        remainingToPay: 0,
-        moneyAdded: [ { id: 2, quantity: 1 } ],
-      },
+      orderedCoffeeName: '',
+      quantityOrdered: 0,
       coins: [
         { id: 1, value: 100, available: 10 },
         { id: 2, value: 500, available: 10 },
         { id: 3, value: 50, available: 10 },
         { id: 4, value: 25, available: 10 },
       ],
+      receiptDetails: {
+        remainingToPay: 0,
+        change: 0,
+        moneyAdded: [ { id: 2, quantity: 1 } ],
+      },
+      cashValueSelected: '',
+      quantityCash: 0,
     }
   },
 
   methods: {
     calculateTotal() {
       var totalToPay = 0;
-      for (let i = 0; i < this.orderCoffees.length; ++i) {
-        totalToPay += this.coffees.find(coffee => coffee.id === this.orderCoffees[i].id).price * this.orderCoffees[i].quantity;
+      for (let i = 0; i < this.orderedCoffees.length; ++i) {
+        totalToPay += this.coffees.find(coffee => coffee.id === this.orderedCoffees[i].id).price * this.orderedCoffees[i].quantity;
       }
       return totalToPay;
     },
 
     sumQuantities() {
       var sumQuantity = 0;
-      for (let i = 0; i < this.orderCoffees.length; ++i) {
-        sumQuantity += this.orderCoffees[i].quantity;
+      for (let i = 0; i < this.orderedCoffees.length; ++i) {
+        sumQuantity += this.orderedCoffees[i].quantity;
       }
       return sumQuantity;
     },
@@ -170,7 +171,7 @@ export default {
 
       this.receiptDetails.remainingToPay = totalMoneyAdded > totalToPay ? 0 : totalToPay - totalMoneyAdded;
 
-      let change = totalMoneyAdded - this.receiptDetails.remainingToPay;
+      let change = totalMoneyAdded - totalToPay;
       if (change < 0) {
         change = 0;
       }
@@ -183,6 +184,50 @@ export default {
         totalMoneyAdded += this.coins.find(coin => coin.id === this.receiptDetails.moneyAdded[i].id).value * this.receiptDetails.moneyAdded[i].quantity;
       }
       return totalMoneyAdded;
+    },
+
+    addToOrder() {
+      let coffeeId = this.coffees.find(coffee => coffee.name === this.orderedCoffeeName).id;
+      let coffeeOrder = this.orderedCoffees.find(coffee => coffee.id === coffeeId);
+
+      if (coffeeOrder != undefined) {
+        coffeeOrder.quantity += Number(this.quantityOrdered);
+      } else {
+        this.orderedCoffees.push({
+          id:       coffeeId,
+          quantity: Number(this.quantityOrdered)
+        });
+      }
+      this.receiptDetails.change = this.calculateChange();
+      
+      this.orderedCoffeeName = '';
+      this.quantityOrdered = 0;
+    },
+
+    addCash() {
+      let coinId = this.coins.find(coin => coin.value === Number(this.cashValueSelected.substring(2))).id;
+      let alreadyAddedCoin = this.receiptDetails.moneyAdded.find(coin => coin.id === coinId);
+
+      if (alreadyAddedCoin != undefined) {
+        alreadyAddedCoin.quantity += Number(this.quantityCash);
+      } else {
+        this.receiptDetails.moneyAdded.push({
+          id:       coinId,
+          quantity: Number(this.quantityCash)
+        });
+      }
+      this.receiptDetails.change = this.calculateChange();
+
+      this.cashValueSelected = '';
+      this.quantityCash = 0;
+    },
+
+    pay() {
+      // Call the http get request
+      this.orderedCoffees = [];
+      this.receiptDetails.change = 0;
+      this.receiptDetails.remainingToPay = 0;
+      this.receiptDetails.moneyAdded = [];
     }
   }
 }
